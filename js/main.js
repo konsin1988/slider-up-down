14bf582f3d1fe0b828f88d0474ceb70e0c523957
konsin1988 <konsin1988@gmail.com>
2024-02-28 16:57:00 +0300

    Change transition

--- js/main.js
+++ js/main.js
@@ -9,11 +9,30 @@ let activeSlideIndex = 0;
 sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
 
 upBtn.addEventListener("click", () => {
+  if (activeSlideIndex === 3) {
+    transitionFast();
+  } else {
+    transitionSlow();
+  }
   changeSlide("up");
 });
 downBtn.addEventListener("click", () => {
+  if (activeSlideIndex === 0) {
+    transitionFast();
+  } else {
+    transitionSlow();
+  }
   changeSlide("down");
 });
+
+function transitionSlow() {
+  sidebar.style.transition = `transform 0.4s`;
+  mainImages.style.transition = `transform 0.4s`;
+}
+function transitionFast() {
+  sidebar.style.transition = `transform 0.1s`;
+  mainImages.style.transition = `transform 0.1s`;
+}
 
 function transform() {
   mainImages.style.transform = `translateY(-${activeSlideIndex * 100}vh)`;
@@ -35,3 +54,24 @@ function changeSlide(direction) {
     transform();
   }
 }
+
+screen.orientation.unlock();
+
+// screen.addEventListener("orientationchange", () => {
+//   console.log("Orientation is " + screen.orientation);
+// });
+
+// // console.log(screen.orientation.type);
+
+window.addEventListener(
+  "orientationchange",
+  function () {
+    if (window.orientation === 90) {
+      screen.orientation.type = "landscape-primary";
+    }
+  },
+  false
+);
+
+// console.log(screen.orientation.type);
+// console.log(window.orientation);
